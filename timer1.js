667fe40bc6c628b29159ee5c752e0e30adf55a34
Elena Chernikova <lena@acvos.com>
2022-02-15 17:02:57 -0800

    eslint done

--- timer1.js
+++ timer1.js
@@ -1,15 +1,12 @@
-const arguments = process.argv.slice(2);
-arguments.sort();
-if (arguments === [] || arguments === NaN) {
-  return;
-} else {
-  for (const argument of arguments){
-    if (argument < 0) {
-      return
-    } else {
-      setTimeout(() => {
-        process.stdout.write('\x07');
-      }, argument * 1000);
-    }
+const args = process.argv.slice(2);
+args.sort();
+
+for (const argument of args) {
+  if (argument < 0 || isNaN(argument)) {
+    continue;
   }
-};
+
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, argument * 1000);
+}
