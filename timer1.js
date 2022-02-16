75392bd89dd9d75d45ad2342b75c1c0d757f027a
Elena Chernikova <lena@acvos.com>
2022-02-15 16:54:55 -0800

    edge cases added

--- timer1.js
+++ timer1.js
@@ -1,7 +1,15 @@
 const arguments = process.argv.slice(2);
 arguments.sort();
-for (const argument of arguments){
-  setTimeout(() => {
-    process.stdout.write('\x07');
-  }, argument * 1000);
-}
+if (arguments === [] || arguments === NaN) {
+  return;
+} else {
+  for (const argument of arguments){
+    if (argument < 0) {
+      return
+    } else {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, argument * 1000);
+    }
+  }
+};
